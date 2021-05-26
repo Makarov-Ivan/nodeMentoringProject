58dbca624eead5a50679940570f3480af9893ca8
Ivan M <makivanik@gmail.com>
2021-05-26 20:20:19 +0300

    model for group enitity

---
+++ src/group/models/group.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+
+const { sequelizeClient } = require("../../../DB_connection/connection");
+
+const Group = sequelizeClient.define("Group", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true
+  },
+  permissions: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+}, {});
+
+module.exports = {
+  groupModule: Group,
+};
