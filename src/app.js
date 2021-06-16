a79f48b31642cc0e838bfa69fc41a9837811e1d7
Ivan M <makivanik@gmail.com>
2021-06-16 20:49:18 +0300

    add uncaughtException to process

--- src/app.js
+++ src/app.js
@@ -9,23 +9,31 @@ var oasDoc = jsyaml.load(spec);
 
 const app = express();
 
-const port=3000;
+const port = 3000;
 
-const oasToolsOptions={
+const oasToolsOptions = {
     controllers: path.normalize(`${__dirname}/`),
     strict: false,
+    loglevel: 'error'
 };
 
 oasTools.configure(oasToolsOptions);
 
-oasTools.initializeMiddleware(oasDoc, app, function(middleware) {
+oasTools.initializeMiddleware(oasDoc, app, function (middleware) {
     // Configuration of usage of swagger middlewares with app.use()
     // console.log(middleware);
     app.use(middleware.swaggerMetadata());
-  
+
     // Start the server
-    app.listen(port, ()=>{
+    app.listen(port, () => {
         console.log("app is listening on ", port);
     });
-  });
+});
 
+process.on('uncaughtException', (err, origin) => {
+    console.log(
+        process.stderr.fd,
+        `Caught exception: ${err}\n` +
+        `Exception origin: ${origin}`
+    );
+});
